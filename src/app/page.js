71125ab8725f137b283546ffa35404f40ff0154f
Yi Xin <angyixin@gmail.com>
2023-07-22 21:01:59 +0800

    playing with blocks

--- src/app/page.js
+++ src/app/page.js
@@ -8,14 +8,14 @@ export default function Home() {
         <div className="grid lg:grid-cols-12 gap-4">
           <div className="lg:col-span-6">
             <div className="lg:py-36 lg:pr-16">
-              <h2 className="text-2xl lg:text-4xl mb-4 lg:mb-8">
+              <h2 className="text-lg lg:text-4xl mb-4 lg:mb-8 text-slate-800">
                 Hello, I am Yi Xin
               </h2>
               <h1 className="text-3xl lg:text-8xl mb-4 lg:mb-12">
                 I solve problems, simplify complex ideas and craft intuitive and
                 delightful experiences.
               </h1>
-              <p className="text-lg lg:text-xl text-slate-700 dark:text-slate-300 lg:leading-relaxed">
+              <p className="text-lg lg:text-2xl text-slate-800 dark:text-slate-300 lg:leading-relaxed">
                 Lead Product Designer at{' '}
                 <strong>Open Government Products, Singapore</strong>. Previously
                 from <strong>Stripe</strong> and{' '}
@@ -25,7 +25,7 @@ export default function Home() {
           </div>
           <div className="lg:col-span-6 flex justify-center items-center">
             <Image src={phoneIntro} height={720} alt="Yi Xin's Mock Phone" />
-            <div className="before:content-[''] before:bg-red-500 before:block before:absolute before:top-0 before:bottom-0 before:right-0 before:lg:w-1/2 before:-z-10"></div>
+            <div className="before:content-[''] before:bg-red-500 before:block before:absolute before:right-0 before:w-[50vw] before:top-0 before:bottom-0 before:-z-10 before:-mx-80"></div>
           </div>
         </div>
       </div>
